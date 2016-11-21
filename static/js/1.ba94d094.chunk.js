b8722745f6482c74a5fa33620a47b5843de2dd39
unknown <h4av.one@gmail.com>
2016-11-21 11:47:06 +0800

    Updates

---
+++ static/js/1.ba94d094.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{150:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(5),o=n(l),r=function(){return o.default.createElement("h1",null,"foo")};t.default=r}});
+//# sourceMappingURL=1.ba94d094.chunk.js.map
---
+++ static/js/2.a2b9d064.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{149:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var r=u(5),l=n(r),a=function(){return l.default.createElement("h1",null,"bar")};t.default=a}});
+//# sourceMappingURL=2.a2b9d064.chunk.js.map
